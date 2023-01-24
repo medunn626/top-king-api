b890f2c309c4053e331da08ba318d59d5abdc6df
Michael Dunn <michaeldunn@mac.attlocal.net>
2023-01-23 21:52:28 -0600

    Allow CORS access to Prod link

--- src/main/java/application/topkingapi/TopKingApiApplication.java
+++ src/main/java/application/topkingapi/TopKingApiApplication.java
@@ -19,7 +19,7 @@ public class TopKingApiApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "https://medunn626.github.io"));
 		corsConfiguration.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
