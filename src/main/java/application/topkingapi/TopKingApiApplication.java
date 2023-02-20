df182e61aa5e443742d75e536376f9e7c11a81e0
Michael Dunn <michaeldunn@mac.attlocal.net>
2023-02-20 11:36:38 -0600

    Add new domain name to allowed origins

--- src/main/java/application/topkingapi/TopKingApiApplication.java
+++ src/main/java/application/topkingapi/TopKingApiApplication.java
@@ -19,7 +19,11 @@ public class TopKingApiApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "https://medunn626.github.io"));
+		corsConfiguration.setAllowedOrigins(List.of(
+				"http://localhost:4200",
+				"https://medunn626.github.io",
+				"https://topkingtraining.com"
+		));
 		corsConfiguration.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
